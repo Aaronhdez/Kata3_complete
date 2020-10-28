fd3c2d6f573ef11fa6f8524733c0ed73bdde3d19
kikenky <aaronhdezalv@hotmail.com>
2020-10-28 11:13:13 +0000

    Primera Versión Kata3 28102020

---
+++ src/Kata3_28102020/HistogramDisplay.java
@@ -0,0 +1,51 @@
+package Kata3_28102020;
+
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+
+public class HistogramDisplay extends ApplicationFrame {
+    
+    public HistogramDisplay(String title) {
+        super(title);
+        this.setContentPane(createPanel());
+        this.pack();
+    }
+
+    private JPanel createPanel() {
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
+        return chartPanel;
+    }
+    
+    private JFreeChart createChart(DefaultCategoryDataset dataSet){
+        JFreeChart chart = ChartFactory.createBarChart("Titulo", 
+                                                       "Email domains", 
+                                                       "Emails Received", 
+                                                       dataSet, 
+                                                       PlotOrientation.VERTICAL, 
+                                                       false, 
+                                                       false, 
+                                                       rootPaneCheckingEnabled);
+        return chart;
+    }
+    
+    private DefaultCategoryDataset createDataset(){
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        dataSet.addValue(23, "", "ulpgc.es");
+        dataSet.addValue(33, "", "gmail.es");
+        dataSet.addValue(13, "", "hotmail.com");
+        dataSet.addValue(15, "", "idec.es");
+        return dataSet; 
+    }
+    
+    public void execute(){
+        this.setVisible(true);
+    }
+    
+    
+    
+}
---
+++ src/Kata3_28102020/Kata3_28102020.java
@@ -0,0 +1,11 @@
+
+package Kata3_28102020;
+
+public class Kata3_28102020 {
+
+    public static void main(String[] args) {
+        HistogramDisplay histo = new HistogramDisplay("HISTOGRAM");
+        histo.execute();
+    }
+    
+}
